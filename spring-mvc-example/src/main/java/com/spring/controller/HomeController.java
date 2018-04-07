897cf739d265e2d5ba60cd3d9c2ecdfa50f4b5e5
nigam <nigam@shashank>
2018-04-07 16:13:18 +0530

    Second Commit

--- spring-mvc-example/src/main/java/com/spring/controller/HomeController.java
+++ spring-mvc-example/src/main/java/com/spring/controller/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.spring.model.User;
-
+//testing for second commit
 @Controller
 public class HomeController {
 
